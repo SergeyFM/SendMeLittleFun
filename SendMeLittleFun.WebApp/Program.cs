fe3fc89caa8f409290c04b72e96e20a8d8cd199d
SergeyFM <github@svistunov.su>
2023-11-15 21:50:18 +0300

    Emails is being send, webserver port = 80

--- SendMeLittleFun.WebApp/Program.cs
+++ SendMeLittleFun.WebApp/Program.cs
@@ -45,11 +45,15 @@ public class Program {
 
 
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment()) {
+        if (app.Environment.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+        } else {
             app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
+        
+        
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
